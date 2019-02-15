1ce38ddfb4d68ddb0fad74ecca6216ec9ef1d827
Nelias <michal.antropik@gmail.com>
2019-02-15 16:36:34 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Account } from './account.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'angular-personal-finance';
+
+  private _accounts: Array<Account> = [
+    {
+      id: 1,
+      title: "Bank Xyz",
+      description: "This is my main bank account.",
+      balance: 500.25
+    },
+
+    new Account(2, "Bank Asd", "My secret account.", 1022.55)
+  ]
+
+  private _nextId = 3;
+  private createAcc(titleEl: any, descEl: any, balEl: any) {
+    this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value));
+    this._nextId++;
+
+    titleEl.value = "";
+    descEl.value = "";
+    balEl.value = 0;
+  }
+
+  private removeAcc(index: number) {
+    this._accounts.splice(index, 1);
+  }
+}
